0d31f09b1afb27170de847ac53254d9b98df04e6
largekim4458 <large@clo3d.com>
2022-12-19 09:51:18 +0900

    :arrow_down: :wrench: svg inline 환경설정 변경

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,28 +1,42 @@
-const path = require('path');
+const path = require("path");
 module.exports = {
-  "stories": [
+  stories: [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    '@storybook/addon-postcss',
+    "@storybook/addon-postcss",
     {
-      name: '@storybook/preset-scss',
+      name: "@storybook/preset-scss",
       options: {
         cssLoaderOptions: {
           modules: true,
-          localIdentName: '[local]__[hash:base64:5]',
-        }
-      }
+          localIdentName: "[local]__[hash:base64:5]",
+        },
+      },
     },
     "storybook-addon-designs",
-    "storybook-addon-next-router"
+    "storybook-addon-next-router",
   ],
-  "framework": "@storybook/react",
+  framework: "@storybook/react",
   core: {
-    builder: 'webpack4'
+    builder: "webpack4",
   },
-}
+  webpackFinal: async (config) => {
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.test(".svg"),
+    );
+    fileLoaderRule.exclude = /\.svg$/;
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      enforce: "pre",
+      loader: require.resolve("@svgr/webpack"),
+    });
+
+    return config;
+  },
+};
